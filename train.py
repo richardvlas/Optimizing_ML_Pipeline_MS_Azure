749fb713210dac52d45b60d5ecfea78a2d2c8055
Richard Vlas <vlasrichard@gmail.com>
2021-07-10 09:18:52 +0000

    changed path

--- train.py
+++ train.py
@@ -70,7 +70,7 @@ def main():
     run.log("Accuracy", np.float(accuracy))
 
     os.makedirs('./outputs', exist_ok=True)
-    joblib.dump(model, 'outputs/model.joblib')
+    joblib.dump(model, './outputs/model.joblib')
 
 if __name__ == '__main__':
     main()
